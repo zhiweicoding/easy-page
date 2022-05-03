8d7288066ca467d9cf7a786f983f7fed254fd2c3
zhiwei <diaozhiwei2k@163.com>
2022-05-03 17:51:57 +0800

    init

---
+++ easy-page-core/src/test/java/io/github/zhiweicoding/AppTest.java
@@ -0,0 +1,17 @@
+package io.github.zhiweicoding;
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
